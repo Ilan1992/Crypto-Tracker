7780c5ad855e7475f046d55e6048c0bd76302088
Ilan1992 <144780128+Ilan1992@users.noreply.github.com>
2024-05-20 15:21:18 +0300

    fix

--- client/src/services/axiosHelper.js
+++ client/src/services/axiosHelper.js
@@ -1,9 +1,15 @@
 import  axios  from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3000/api";
+const baseUrl =
+  import.meta.env.VITE_SERVER_PORT ||
+  "https://crypto-tracker-59kr.onrender.com";
+
+
 const baseApi = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${baseUrl}/api`,
 });
+
 
 
 baseApi.interceptors.request.use((config)=>{
